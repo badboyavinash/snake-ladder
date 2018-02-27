32835649eddc817e35e78d14334e118aecf8e833
badboyavinash <35717757+badboyavinash@users.noreply.github.com>
2018-02-27 10:12:58 +0530

    Add files via upload

---
+++ program9.py
@@ -0,0 +1,13 @@
+print("example for continue using strings") 
+a="python"
+for i in a:
+    if (i=='t'):
+        continue
+    print(i)
+
+print("example for break using strings")
+for i in a:
+    if (i=="o"):
+        break
+    print(i)
+   
